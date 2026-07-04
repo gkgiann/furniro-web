9b6c21ca47110afee44ef2c901925b1be58ba78f
gkgiann <lgian584@gmail.com>
2026-07-04 18:00:17 -0300

    refactor: sort tailwind classes

--- src/components/Discover/Discover.tsx
+++ src/components/Discover/Discover.tsx
@@ -7,28 +7,20 @@ export function Discover() {
       <img src={interior1} alt="Imagem de interior de sala" />
       <div
         className={clsx(
-          "absolute inset-x-0",
-          "mx-auto -mt-14 max-w-2xs p-6 pt-7",
-          "flex flex-col",
-          "rounded-[10px] bg-yellow-primary-light",
+          "absolute inset-x-0 mx-auto -mt-14 flex max-w-2xs flex-col rounded-[10px] bg-yellow-primary-light p-6 pt-7",
           "sm:-mt-30 sm:max-w-lg",
           "md:-mt-40 md:max-w-160.75",
           "lg:right-10 lg:bottom-12 lg:left-auto lg:mx-0 lg:mt-0 lg:p-7 lg:pt-10",
           "xl:right-15.75 xl:bottom-29.25 xl:p-9.25 xl:pt-14",
         )}
       >
-        <span
-          className={clsx(
-            "mb-1 text-sm font-semibold tracking-[3px] text-primary",
-            "xl:text-base",
-          )}
-        >
+        <span className="mb-1 text-sm font-semibold tracking-[3px] text-primary xl:text-base">
           New Arrival
         </span>
         <h1
           className={clsx(
-            "mb-2 text-2xl font-bold text-yellow-primary",
-            "leading-8 xl:mb-4.25 xl:text-[52px] xl:leading-16.25",
+            "mb-2 text-2xl leading-8 font-bold text-yellow-primary",
+            "xl:mb-4.25 xl:text-[52px] xl:leading-16.25",
           )}
         >
           Discover Our <br /> New Collection
@@ -45,12 +37,9 @@ export function Discover() {
 
         <button
           className={clsx(
-            "mx-auto w-fit px-16 py-4",
-            "bg-yellow-primary text-sm font-bold text-white",
-            "cursor-pointer transition",
-            "lg:mx-0",
-            "xl:px-18 xl:py-6.25 xl:text-base",
-            "hover:scale-105",
+            "mx-auto w-fit cursor-pointer bg-yellow-primary px-16 py-4 text-sm font-bold text-white transition hover:scale-105",
+            "lg:mx-0 xl:px-18",
+            "xl:py-6.25 xl:text-base",
           )}
         >
           BUY NOW
